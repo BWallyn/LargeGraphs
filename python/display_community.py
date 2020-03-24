45e438bc247b8f9c35be73b323f68a20ab4326d5
Benjamin WALLYN <benjamin.wallyn@ensta-paristech.fr>
2020-03-24 11:36:56 +0100

    add heatmaps for the results

--- python/display_community.py
+++ python/display_community.py
@@ -18,15 +18,29 @@ def display_community(input_file):
 	"""
 
 	df = pd.read_csv(input_file, float_precision='round_trip')
-	plt.figure(num=1)
+	df['p_divided_by_q'] = df['p']/df['q']
 	sns.relplot(x="q", y="number_community",
 				hue="p",
 				size="running_time", #size_norm=(),
 				palette="muted",
 				col="method",
 				data=df)
+	sns.relplot(x="p_divided_by_q", y="number_community",
+				hue="method",
+				size="running_time", #size_norm=(),
+				palette="muted",
+				col="method",
+				data=df)
+	
+	# Heatmap
+	result_label = df[df["method"] == 'Label'].pivot(index="p", columns="q", values="number_community")
+	fig_lab, ax_lab = plt.subplots()
+	ax_lab = sns.heatmap(result_label)
+
+	result_louvain = df[df["method"] == 'Louvain'].pivot(index="p", columns="q", values="number_community")
+	fig_lou, ax_lou = plt.subplots()
+	ax_lou = sns.heatmap(result_louvain)
 	plt.show()
-	plt.close(1)
 
 
 # =============
